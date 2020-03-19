7e31259a39a1a18fc5000077a3c4b1673779a430
Erisvan Junior <erisvan.junior.a@gmail.com>
2020-03-19 00:36:04 -0300

    feat: Loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Loading() {
+  return (
+    <Container>
+      <div />
+      <div />
+    </Container>
+  );
+}
---
+++ src/components/Loading/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  div {
+    position: absolute;
+    border: 4px solid #179;
+    opacity: 1;
+    border-radius: 50%;
+    animation: animationSpin 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+
+  div:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+
+  @keyframes animationSpin {
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: 0px;
+      left: 0px;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
+    }
+  }
+`;
